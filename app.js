7b78d45047a6fe4bd96c25fd79dba18e0d7a4c84
deniaverkade <9024406@student.zadkine.nl>
2024-12-10 12:39:47 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+//opracht 1
+
+/* for (let i =0; i <101; i++) {
+    console.log(i);
+} */
+
+//opdracht 2
+
+/* for (let i =100; i >-1 ; i--) {
+    console.log(i);
+} */ 
+
+    // Code staat in commentaar omdat als beide niet in commentaar staan dan is er een oneindige loop die misschien je browser kan crashen.
